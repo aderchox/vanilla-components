3eb3db6e4fd9e3c61e011008db3c356a49ca2a32
aderchox <aderchox@gmail.com>
2022-12-07 14:17:32 +0330

    chore: Adds interactivity to the star rating and addas "rating-sticky" and "rating-readonly" types to the component.

--- star-rating/star-rating.js
+++ star-rating/star-rating.js
@@ -1,23 +1,55 @@
 function constructStarRating(element) {
   const stars = makeStars();
   let _roundedRating;
+  let _locked;
 
   (function main() {
+    if (element.hasAttribute("rating-readonly")) {
+      update();
+      return;
+    }
     const observer = new MutationObserver(function handleMutation(mutations) {
       mutations.forEach(function (mutation) {
         if (mutation.type === "attributes") {
-          setRoundedRating();
-          render();
+          update();
         }
       });
     });
 
-    setRoundedRating();
-    render();
+    initEventListeners();
+    update();
     observer.observe(element, {
       attributes: true,
     });
   })();
+
+  function initEventListeners() {
+    for (let i = 0; i < stars.length; i++) {
+      const star = stars[i];
+      star.addEventListener(
+        "mouseenter",
+        function mouseEnterHandler(mouseEnterEvent) {
+          update(i + 1);
+        }
+      );
+      star.addEventListener("click", function clickHandler(clickEvent) {
+        if (_locked) {
+          return;
+        }
+        // Triggers the mutation observer too automatically
+        element.dataset.rating = i + 1;
+        if (element.hasAttribute("rating-sticky")) {
+          _locked = true;
+        }
+      });
+    }
+    element.addEventListener(
+      "mouseleave",
+      function mouseLeaveHandler(mouseLeaveEvent) {
+        update();
+      }
+    );
+  }
 
   function makeStars() {
     const stars = [];
@@ -50,8 +82,11 @@ function constructStarRating(element) {
     return 0;
   }
 
-  function render() {
-    const ratingUpperStarNumber = _roundedRating;
+  function render(topFilledStarNumber) {
+    if (_locked) {
+      return;
+    }
+    const ratingUpperStarNumber = topFilledStarNumber || _roundedRating;
     for (const [index, star] of stars.entries()) {
       if (index < ratingUpperStarNumber) {
         star.innerHTML = "x";
@@ -59,6 +94,11 @@ function constructStarRating(element) {
         star.innerHTML = "o";
       }
     }
+  }
+
+  function update(topFilledStarNumber) {
+    topFilledStarNumber || setRoundedRating();
+    render(topFilledStarNumber);
   }
 }
 
