ffd33768a3e965057d68bd2ffd0940dd4230c6da
aderchox <aderchox@gmail.com>
2023-02-16 16:11:20 +0330

    feat: Adds fading and sliding animations to the modal component. 🤾‍ 	refactor: Uses constants instead of literal string class names in the modal script. 	style: Adds more comments to the modal component's code.

--- modal/modal.js
+++ modal/modal.js
@@ -1,8 +1,15 @@
 // Assumed Class Names
-const modalOpenerClass = "modalOpener";
-const modalBodyClass = "modalBody";
-const modalWrapper = document.querySelector(".modalWrapper");
-const modalPlace = document.querySelector(".modalPlace");
+const MODAL_OPENER = "modalOpener";
+const MODAL_BODY = "modalBody";
+const MODAL_ACTIVE = "modalActive";
+const MODAL_CLOSING = "modalClosing";
+const MODAL_WRAPPER = "modalWrapper";
+const MODAL_PLACE = "modalPlace";
+const MODAL_WRAPPER_HIDDEN = "hidden";
+
+// The place where active modals will be placed in, and its wrapper.
+const modalPlace = document.querySelector(`.${MODAL_PLACE}`);
+const modalWrapper = document.querySelector(`.${MODAL_WRAPPER}`);
 
 function constructModal(container, onOpen, onClose) {
   (function main() {
@@ -10,25 +17,44 @@ function constructModal(container, onOpen, onClose) {
   })();
 
   function initEventListeners() {
-    const openerButton = container.querySelector(`.${modalOpenerClass}`);
+    const openerButton = container.querySelector(`.${MODAL_OPENER}`);
+    const modalBody = container.querySelector(`.${MODAL_BODY}`);
     openerButton.addEventListener("click", function openModal(clickEvent) {
-      modalWrapper.classList.remove("hidden");
-      const modalBody = container.querySelector(`.${modalBodyClass}`);
+      // Do not open a modal while another one is half-way through of being closed.
+      if (modalWrapper.classList.contains(MODAL_CLOSING)) {
+        return;
+      }
+      modalWrapper.classList.remove(MODAL_WRAPPER_HIDDEN);
+      // Move the requested modalBody from its container to the modalPlace.
       modalPlace.prepend(modalBody);
-      container.classList.add("modalActive");
-      onOpen(modalPlace, container);
+      // Highlight which container's modalBody is being shown.
+      container.classList.add(MODAL_ACTIVE);
+      // Run the user-requested onOpen callback.
+      onOpen && onOpen(modalPlace, container);
     });
     modalWrapper.addEventListener("click", function closeModal(clickEvent) {
-      const clickedOutOfModal = !clickEvent.target.closest(".modalPlace");
+      // This event listener triggers for all "container"s, so the ones that are not the active one must ignore it.
+      if (!container.classList.contains(MODAL_ACTIVE)) {
+        return;
+      }
+      const clickedOutOfModal = !clickEvent.target.closest(`.${MODAL_PLACE}`);
       if (clickedOutOfModal) {
-        // This event listener triggers for all "container"s, so the ones that are not the active one must ignore it.
-        if (!container.classList.contains("modalActive")) {
-          return;
-        }
-        modalWrapper.classList.add("hidden");
-        container.classList.remove("modalActive");
-        container.prepend(modalPlace.firstChild);
-        onClose(modalPlace, container);
+        modalWrapper.classList.add(MODAL_CLOSING);
+        // Better grab the currentModalBody here, instead of within the event listener's callback, because at that point, it could have been changed.
+        const currentModalBody = modalPlace.firstChild;
+        // Run it only once ({once: true}) and then kill it, or else will run on next fade-in animation too, and the modal will never be able to completely re-appear.
+        modalWrapper.addEventListener(
+          "animationend",
+          function returnModalBodyToItsContainer(e) {
+            container.prepend(currentModalBody);
+            container.classList.remove(MODAL_ACTIVE);
+            modalWrapper.classList.remove(MODAL_CLOSING);
+            modalWrapper.classList.add(MODAL_WRAPPER_HIDDEN);
+          },
+          { once: true }
+        );
+        // Run the user-requested onClose callback.
+        onClose && onClose(modalPlace, container);
       }
     });
   }
